252c44b4b58b372d0dc69cbeff4dce57b853f953
Md. Rashed - Uz - Zaman <rashedgit@gmail.com>
2021-02-13 11:19:50 +0600

    EasySwiftUIKit Version - 1.0.3.04
    EasySwiftUIKit Version - 1.0.3.04

---
+++ Source/MimeType.swift
@@ -0,0 +1,24 @@
+//
+//  MimeType.swift
+//  EasySwiftUIKit
+//
+//  Created by Rz Rasel on 2021-02-13
+//
+
+import Foundation
+
+extension NSURL {
+    public func mimeType() -> String {
+        return MimeType(ext: self.pathExtension)
+    }
+}
+extension NSString {
+    public func mimeType() -> String {
+        return MimeType(ext: self.pathExtension)
+    }
+}
+extension String {
+    public func mimeType() -> String {
+        return (self as NSString).mimeType()
+    }
+}
