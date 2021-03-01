09000e961a8c6efa9e57ad171f4ff16091687ec8
arzrasel <rashed@gmail.com>
2021-03-01 13:26:16 +0600

    WKWebView
    WKWebView

--- Source/WKWebViewExtensions.swift
+++ Source/WKWebViewExtensions.swift
@@ -29,7 +29,7 @@ extension WKWebView {
         loadHTMLString(htmlString, baseURL: Bundle.main.resourceURL)
     }
 }
-func getWebViewUrl(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) -> String! {
+public func getWebViewUrl(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) -> String! {
     var urlStr: String! = nil
     if let data = navigationAction.request.url?.absoluteString {
         //urlStr is what you want
