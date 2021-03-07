962bed121b11eb87eacc29e109138204b4f46b8a
arzrasel <rashedgit@gmail.com>
2021-03-07 13:53:59 +0600

    EasySwiftUIKit Version - 1.0.4.05
    EasySwiftUIKit Version - 1.0.4.05

--- Source/JSONExtensions.swift
+++ Source/JSONExtensions.swift
@@ -100,4 +100,76 @@ open class JSONParser {
 //            return (false, nil)
 //        }
     }
+    public static func parse<T: Codable>(dataModel: T.Type, jsonData argJSONData: Data) -> Result<T, Error> {
+        return Result { try JSONDecoder().decode(dataModel.self, from: JSONSerialization.data(withJSONObject: argJSONData)) }
+//        do {
+//            let jsonData = try JSONSerialization.data(withJSONObject: argJSONData)
+//            let decoder = JSONDecoder()
+//            let modelData = try decoder.decode(dataModel.self, from: jsonData)
+//            return (true, modelData)
+//        } catch {
+//            return (false, nil)
+//        }
+    }
+    public static func parse<T: Codable>(dataModel: [T].Type, jsonData argJSONData: Data) -> Result<[T], Error> {
+        return Result { try JSONDecoder().decode(dataModel.self, from: JSONSerialization.data(withJSONObject: argJSONData)) }
+//        do {
+//            let jsonData = try JSONSerialization.data(withJSONObject: argJSONData)
+//            let decoder = JSONDecoder()
+//            let modelData = try decoder.decode(dataModel.self, from: jsonData)
+//            return (true, modelData)
+//        } catch {
+//            return (false, nil)
+//        }
+    }
+    public static func parse<T: Codable>(dataModel: T.Type, jsonData argJSONData: Any) -> Result<T, Error> {
+        return Result { try JSONDecoder().decode(dataModel.self, from: JSONSerialization.data(withJSONObject: argJSONData)) }
+//        do {
+//            let jsonData = try JSONSerialization.data(withJSONObject: argJSONData)
+//            let decoder = JSONDecoder()
+//            let modelData = try decoder.decode(dataModel.self, from: jsonData)
+//            return (true, modelData)
+//        } catch {
+//            return (false, nil)
+//        }
+    }
+    public static func parse<T: Codable>(dataModel: [T].Type, jsonData argJSONData: Any) -> Result<[T], Error> {
+        return Result { try JSONDecoder().decode(dataModel.self, from: JSONSerialization.data(withJSONObject: argJSONData)) }
+//        do {
+//            let jsonData = try JSONSerialization.data(withJSONObject: argJSONData)
+//            let decoder = JSONDecoder()
+//            let modelData = try decoder.decode(dataModel.self, from: jsonData)
+//            return (true, modelData)
+//        } catch {
+//            return (false, nil)
+//        }
+    }
+    public static func parse<T: Codable>(dataModel: T.Type, jsonData argJSONData: String) -> Result<T, Error> {
+        return Result { try JSONDecoder().decode(dataModel.self, from: JSONSerialization.data(withJSONObject: Data(argJSONData.utf8))) }
+//        do {
+////            let jsonData = try JSONSerialization.data(withJSONObject: jsonData)
+//            let rawData = Data(argJSONData.utf8)
+////            let jsonData = try JSONSerialization.jsonObject(with: rawData, options: [])
+//            let jsonData = try JSONSerialization.data(withJSONObject: rawData, options: [])
+//            let decoder = JSONDecoder()
+//            let modelData = try decoder.decode(dataModel.self, from: jsonData)
+//            return (true, modelData)
+//        } catch {
+//            return (false, nil)
+//        }
+    }
+    public static func parse<T: Codable>(dataModel: [T].Type, jsonData argJSONData: String) -> Result<[T], Error> {
+        return Result { try JSONDecoder().decode(dataModel.self, from: JSONSerialization.data(withJSONObject: Data(argJSONData.utf8))) }
+//        do {
+////            let jsonData = try JSONSerialization.data(withJSONObject: jsonData)
+//            let rawData = Data(argJSONData.utf8)
+////            let jsonData = try JSONSerialization.jsonObject(with: rawData, options: [])
+//            let jsonData = try JSONSerialization.data(withJSONObject: rawData, options: [])
+//            let decoder = JSONDecoder()
+//            let modelData = try decoder.decode(dataModel.self, from: jsonData)
+//            return (true, modelData)
+//        } catch {
+//            return (false, nil)
+//        }
+    }
 }
